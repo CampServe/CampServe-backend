1da9e8ff23a881bd6f2d96e1eda82c16a70906de
joforikwakye <jeffkwakye4u@gmail.com>
2023-05-27 16:50:43 +0000

    this folder contains the student models and services

---
+++ backend/Students/StudentModel.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String, Integer, Column
+from sqlalchemy.orm import relationship
+
+
+from base import Base
+
+
+class Students(Base):
+    __tablename__ = "students"
+    student_id = Column("student_id",Integer,primary_key = True,nullable= False)
+    first_name = Column("first_name",String(255),nullable=False)
+    last_name = Column("last_name",String(255),nullable=False)
+    username = Column("username",String(255),nullable=False)
+    ref_number = Column("ref_number",String(255),nullable=False)
+    password = Column("password",String(255), nullable = False)
+
+#relationships
+transactions = relationship("transactions",uselist = False,back_populates="transactions")
+ratings = relationship("ratings", uselist=False,back_populates = "ratings")
+provider = relationship("providers",uselist=False,back_populates="providers")
