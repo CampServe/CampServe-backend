6e6ec348568ebdca97ae373abe2683085e9e26c7
joforikwakye <jeffkwakye4u@gmail.com>
2023-06-05 19:32:52 +0000

    updated the relationships and added the user id as a foreign key

--- backend/Providers/ProviderModel.py
+++ backend/Providers/ProviderModel.py
@@ -1,13 +1,14 @@
 from sqlalchemy import String, Integer, Column, ForeignKey
 from sqlalchemy.orm import relationship
 from Students.StudentModel import Students
+from Users.UserModel import User
 
 from base import Base
 
 class Providers(Base):
     __tablename__ = "providers"
     provider_id = Column("provider_id",Integer, primary_key=True, autoincrement=True)
-    student_id = Column("student_id",Integer, ForeignKey(Students.student_id), nullable=False)
+    user_id = Column("user_id",Integer, ForeignKey(User.user_id), nullable=False)
     profile_pic = Column("profile_pic",String(255), nullable=True, default=None)
     banner_image = Column("banner_image",String(255), nullable=True, default=None)
     provider_contact = Column("provider_contact",String(255), nullable=True, default=None)
@@ -18,3 +19,4 @@ class Providers(Base):
 # Relationships
 ratings = relationship("Ratings", uselist=False, back_populates="provider")
 product = relationship("Product", uselist=False, back_populates="provider")
+users = relationship("Users", uselist=False, back_populates="users")
