7dde2a493e7cce29a347a9344e69cf64c6bfd476
joforikwakye <jeffkwakye4u@gmail.com>
2023-06-07 15:03:23 +0000

    added a token generation for users when they log in

--- backend/Users/UserService.py
+++ backend/Users/UserService.py
@@ -1,26 +1,27 @@
-from lib2to3.pgen2 import token
 from flask_cors import CORS
-from flask import jsonify, Blueprint, request,session,current_app
+from flask import jsonify, Blueprint, request, session
 from Students.StudentModel import Students
 from werkzeug.security import generate_password_hash, check_password_hash
 from Users.UserModel import User
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
 
 
 users_route = Blueprint("users_route", __name__)
 CORS(users_route)
 
-@users_route.route("/add_user",methods=['POST'])
+
+@users_route.route("/add_user", methods=['POST'])
 def add_user():
     from app import session as s
     first_name = request.json['first_name']
     last_name = request.json['last_name']
     username = request.json['username']
     password = request.json['password']
-    
-    
+
     if 'email' in session:
         user_email = session['email']
-       
 
     hashed_password = generate_password_hash(password)
 
@@ -29,9 +30,10 @@ def add_user():
         result = {
             'status': 'user already exists'
         }
- 
-    else: 
-        user = User(first_name=first_name, last_name=last_name, username=username, password=hashed_password,email=user_email,is_service_provider=False)
+
+    else:
+        user = User(first_name=first_name, last_name=last_name, username=username,
+                    password=hashed_password, email=user_email, is_service_provider=False)
         s.add(user)
         s.commit()
 
@@ -41,9 +43,11 @@ def add_user():
     return jsonify(result)
 
 
-@users_route.route("/user_login",methods=['POST'])
+
+@users_route.route("/user_login", methods=['POST'])
 def login():
     from app import session
+    from app import app
     username = request.json['username']
     password = request.json['password']
 
@@ -51,25 +55,35 @@ def login():
     if user:
         verify = check_password_hash(pwhash=user.password, password=password)
         if verify:
+
+            #generate token
+            token = jwt.encode({
+                'user': username,
+                'user_id': user.user_id,
+                'expiration': str(datetime.utcnow() + timedelta(days=1))
+            },
+                app.config['SECRET_KEY'])
+
             result = {
                 'status': 'Login successful',
                 'first_name': user.first_name,
                 'last_name': user.last_name,
                 'username': user.username,
                 'email': user.email,
-                'user_id':user.user_id,
+                'user_id': user.user_id,
                 'is_service_provider': user.is_service_provider,
-                'account_type': 'regular user'
+                'account_type': 'regular user',
+                'token': token
             }
 
-            #this is where you generate the token. the token will have information on who the user is and what to send back
             return jsonify(result)
         else:
             result = 'Incorrect username or password'
             return jsonify(result)
     else:
         # Check if the username exists in the database
-        existing_user = session.query(User).filter_by(username=username).first()
+        existing_user = session.query(
+            User).filter_by(username=username).first()
         if existing_user:
             result = 'Incorrect password'
         else:
@@ -79,4 +93,31 @@ def login():
 
 
 
+# def token_required(f):
+#     @wraps(f)
+#     def decorated(*args, **kwargs):
+#         from app import app
+#         token = None
+#         # jwt is passed in the request header
+#         if 'x-access-token' in request.headers:
+#             token = request.headers['x-access-token']
+
+#         # return 401 if token is not passed
+#         if not token:
+#             return jsonify({'message' : 'Token is missing !!'})
+  
+#         try:
+#             # decoding the payload to fetch the stored details
+#             data = jwt.decode(token, app.config['SECRET_KEY'])
+#             current_user = User.query\
+#                 .filter_by(username = data['username'])\
+#                 .first()
+#         except:
+#             return jsonify({
+#                 'message' : 'Token is invalid !!'
+#             }), 401
+#         # returns the current logged in users context to the routes
+#         return  f(current_user, *args, **kwargs)
+  
+#     return decorated
 
