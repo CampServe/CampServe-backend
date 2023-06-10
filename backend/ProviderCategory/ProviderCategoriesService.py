b209623a3a4478b0b18639e6ea7f4fb7f3b712f0
joforikwakye <jeffkwakye4u@gmail.com>
2023-06-10 16:07:43 +0000

    added a route that returns all the services offered by providers

--- backend/ProviderCategory/ProviderCategoriesService.py
+++ backend/ProviderCategory/ProviderCategoriesService.py
@@ -1,11 +1,40 @@
 from flask_cors import CORS
 from flask import jsonify, Blueprint, request
+from ProviderCategory.ProviderCategoriesModel import ProviderCategories
+
+from Providers.ProviderModel import Providers
 
 
 provider_categories_route = Blueprint("provider_categories_route", __name__)
 CORS(provider_categories_route)
 
 
-#2 routs
-#1. to return all the main categories associated with a particular student id
-#2. to return all the sub categories associated with a particular student id
+@provider_categories_route.route("/get_all_services", methods=['GET'])
+def get_services():
+    from app import session
+
+    data = session.query(
+        Providers.user_id,
+        Providers.provider_contact,
+        Providers.business_name,
+        Providers.bio,
+        ProviderCategories.main_categories,
+        ProviderCategories.sub_categories
+    ).all()
+
+    result = {
+        'data': [
+            {
+                'user_id': user_id,
+                'provider_contact': provider_contact,
+                'business_name': business_name,
+                'bio': bio,
+                'main_categories': main_categories,
+                'sub_categories': sub_categories
+
+            }
+            for user_id,  provider_contact, business_name, bio, main_categories, sub_categories, in data
+        ]
+    }
+
+    return jsonify(result)
