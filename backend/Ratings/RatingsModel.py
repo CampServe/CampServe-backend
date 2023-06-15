ca0ef3ea58c55bf0fe356beb8cfebb06315b7447
joforikwakye <jeffkwakye4u@gmail.com>
2023-06-15 15:55:39 +0000

    added a rating id

--- backend/Ratings/RatingsModel.py
+++ backend/Ratings/RatingsModel.py
@@ -1,6 +1,7 @@
 from sqlalchemy import String, Integer, Column, ForeignKey
 from sqlalchemy.orm import relationship
 from Providers.ProviderModel import Providers
+from Users.UserModel import User
 
 
 from base import Base
@@ -8,10 +9,11 @@ from base import Base
 
 class Ratings(Base):
     __tablename__ = "ratings"
-    rating_id = Column(Integer, primary_key=True, nullable=False)
-    provider_id = Column(Integer, ForeignKey(Providers.provider_id), nullable=False)
-    no_of_stars = Column(Integer, nullable=False)
-    comments = Column(String(255), nullable=False)
+    rating_id = Column("rating_id", Integer, primary_key=True, nullable=False)
+    provider_id = Column("provider_id",Integer, ForeignKey(Providers.provider_id), nullable=False)
+    user_id = Column("user_id",Integer,ForeignKey(User.user_id),nullable=False)
+    no_of_stars = Column("no_of_stars",Integer, nullable=False)
+    comments = Column("comments", String(255), nullable=False)
 
 # Relationships
 provider = relationship("Providers", back_populates="ratings")
