b7d24b76994c63233f6751ef4d2185ed74bbf82a
joforikwakye <jeffkwakye4u@gmail.com>
2023-08-01 13:58:49 +0000

    added a new column to the model

--- backend/Transactions/TransactionModel.py
+++ backend/Transactions/TransactionModel.py
@@ -13,6 +13,7 @@ class Transactions(Base):
     transaction_id = Column("transaction_id",Integer, primary_key=True,nullable=False)
     request_id = Column("request_id",Integer, ForeignKey(Requests.request_id),nullable=False)
     provider_id = Column("provider_id",Integer, ForeignKey(Providers.provider_id),nullable=False)
+    user_id = Column("user_id",Integer, ForeignKey(User.user_id),nullable=False)
     amount = Column("amount",String(255),nullable = False)
     has_paid =  Column("has_paid",String(255),nullable = True)
     recepient_number=Column("recepient_number",String(255),nullable=False)
